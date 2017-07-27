7567b78a167215aa9c9258dde13a9b0418f84b9d
AussieGuy0 <anthony.bruno196@gmail.com>
2017-07-27 22:56:05 +0930

    Add Weather class

---
+++ src/main/java/me/anthonybruno/personalnewsletter/weather/Weather.java
@@ -0,0 +1,12 @@
+package me.anthonybruno.personalnewsletter.weather;
+
+public class Weather {
+
+    private final int temp;
+    private final String type;
+
+    public Weather(int temp, String type) {
+        this.temp = temp;
+        this.type = type;
+    }
+}
