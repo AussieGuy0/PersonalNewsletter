b29cd370611501b23b853dbd8d6791d020e3fe45
AussieGuy0 <anthony.bruno196@gmail.com>
2017-07-30 15:08:41 +0930

    Implement getWeekForecast in OpenWeatherApiService

--- src/main/java/me/anthonybruno/personalnewsletter/weather/OpenWeatherApiService.java
+++ src/main/java/me/anthonybruno/personalnewsletter/weather/OpenWeatherApiService.java
@@ -1,5 +1,6 @@
 package me.anthonybruno.personalnewsletter.weather;
 
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import me.anthonybruno.personalnewsletter.JsonResponse;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -10,12 +11,15 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class OpenWeatherApiService implements WeatherService {
 
-    private static final String API_URL = "http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&APPID={appId}&units=metric"; //TODO: Make units settable
+    private static final String BASE_URL = "http://api.openweathermap.org/data/2.5/";
+    private static final String CURRENT_WEATHER_URL = BASE_URL + "weather?lat={lat}&lon={lon}&APPID={appId}&units=metric"; //TODO: Make units settable
+    private static final String FORECAST_URL = BASE_URL + "forecast?lat={lat}&lon={lon}&APPID={appId}&units=metric"; //TODO: Make units settable
 
     private CloseableHttpClient httpClient =  HttpClientBuilder.create().build();
     private final String apiKey;
@@ -24,22 +28,16 @@ public class OpenWeatherApiService implements WeatherService {
         this.apiKey = apiKey;
     }
 
-    private String createUrl(double latitude, double longitude) {
-        return API_URL.replace("{lat}", latitude + "").replace("{lon}", longitude + "").replace("{appId}", apiKey);
-    }
 
     @Override
     public Weather getTodaysWeather(Location location) {
         Weather weather;
-        HttpUriRequest request = new HttpGet(createUrl(location.getLatitude(), location.getLongitude()));
+        HttpUriRequest request = new HttpGet(createUrl(CURRENT_WEATHER_URL, location.getLatitude(), location.getLongitude()));
         try {
             try (CloseableHttpResponse r = httpClient.execute(request)) {
                 JsonResponse<ObjectNode> response = new JsonResponse<>(r);
                 ObjectNode node = response.getJson();
-                int temp = node.get("main").get("temp").asInt();
-                String type = node.get("weather").get(0).get("main").asText();
-                weather = new Weather(temp, type);
-
+                weather = convertResponseObjectToWeather(node);
             }
         } catch (IOException e) {
             throw new IllegalArgumentException(e);
@@ -53,7 +51,35 @@ public class OpenWeatherApiService implements WeatherService {
     }
 
     @Override
-    public List<Weather> getWeekForecast(Location location) { //TODO: This
-        throw new NotImplementedException();
+    public List<Weather> getWeekForecast(Location location) {
+        List<Weather> list = new ArrayList<>();
+        HttpUriRequest request = new HttpGet(createUrl(FORECAST_URL, location.getLatitude(), location.getLongitude()));
+        try {
+            try (CloseableHttpResponse r = httpClient.execute(request)) {
+                JsonResponse<ObjectNode> response = new JsonResponse<>(r);
+                ObjectNode node = response.getJson();
+                ArrayNode weatherNodeList = (ArrayNode) node.get("list");
+
+                weatherNodeList.forEach(o -> list.add(convertResponseObjectToWeather((ObjectNode) o))); //FIXME: Filter so only one weather per day
+            }
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+        return list;
     }
+
+    private Weather convertResponseObjectToWeather(ObjectNode node) {
+        int temp = node.get("main").get("temp").asInt();
+        String type = node.get("weather").get(0).get("main").asText();
+        return new Weather(temp, type);
+    }
+
+    private String createUrl(String url, double latitude, double longitude) {
+        return url.replace("{lat}", latitude + "").replace("{lon}", longitude + "").replace("{appId}", apiKey);
+    }
+
+    private HttpGet createGetRequest(String url, Location location) {
+       return new HttpGet(createUrl(url, location.getLatitude(), location.getLongitude()));
+    }
+
 }
